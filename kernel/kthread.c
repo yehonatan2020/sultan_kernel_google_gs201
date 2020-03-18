1d545ea2d494241a8fa96f6a7cb6fece8a184d4d
Park Ju Hyung <qkrwngud825@gmail.com>
2020-03-18 20:23:03 +0900

    kthread: use buffer from the stack space
    struct kthread_create_info is small enough to fit perfectly under
    the stack space.
    
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- kernel/kthread.c
+++ kernel/kthread.c
@@ -363,18 +363,15 @@ struct task_struct *__kthread_create_on_node(int (*threadfn)(void *data),
 {
 	DECLARE_COMPLETION_ONSTACK(done);
 	struct task_struct *task;
-	struct kthread_create_info *create = kmalloc(sizeof(*create),
-						     GFP_KERNEL);
+	struct kthread_create_info create;
 
-	if (!create)
-		return ERR_PTR(-ENOMEM);
-	create->threadfn = threadfn;
-	create->data = data;
-	create->node = node;
-	create->done = &done;
+	create.threadfn = threadfn;
+	create.data = data;
+	create.node = node;
+	create.done = &done;
 
 	spin_lock(&kthread_create_lock);
-	list_add_tail(&create->list, &kthread_create_list);
+	list_add_tail(&create.list, &kthread_create_list);
 	spin_unlock(&kthread_create_lock);
 
 	wake_up_process(kthreadd_task);
@@ -389,7 +386,7 @@ struct task_struct *__kthread_create_on_node(int (*threadfn)(void *data),
 		 * calls complete(), leave the cleanup of this structure to
 		 * that thread.
 		 */
-		if (xchg(&create->done, NULL))
+		if (xchg(&create.done, NULL))
 			return ERR_PTR(-EINTR);
 		/*
 		 * kthreadd (or new kernel thread) will call complete()
@@ -397,7 +394,7 @@ struct task_struct *__kthread_create_on_node(int (*threadfn)(void *data),
 		 */
 		wait_for_completion(&done);
 	}
-	task = create->result;
+	task = create.result;
 	if (!IS_ERR(task)) {
 		char name[TASK_COMM_LEN];
 
@@ -408,7 +405,6 @@ struct task_struct *__kthread_create_on_node(int (*threadfn)(void *data),
 		vsnprintf(name, sizeof(name), namefmt, args);
 		set_task_comm(task, name);
 	}
-	kfree(create);
 	return task;
 }
 
